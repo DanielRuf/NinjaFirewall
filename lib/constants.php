b4f5a5b6666fe05cedf36a24dc410bbe4bd8f863
DanielRuf <daniel@daniel-ruf.de>
2016-05-22 22:42:05 +0200

    NinjaFirewall Pro Edition 3.1.4

--- lib/constants.php
+++ lib/constants.php
@@ -6,7 +6,7 @@
  | (c) NinTechNet - http://nintechnet.com/                             |
  |                                                                     |
  +---------------------------------------------------------------------+
- | REVISION: 2016-04-16 17:00:50                                       |
+ | REVISION: 2016-05-19 11:41:55                                       |
  +---------------------------------------------------------------------+
  | This program is free software: you can redistribute it and/or       |
  | modify it under the terms of the GNU General Public License as      |
@@ -19,11 +19,10 @@
  | GNU General Public License for more details.                        |
  +---------------------------------------------------------------------+
 */
-
 /* ------------------------------------------------------------------ */
 
-define('NFW_ENGINE_VERSION', '3.1.3');
-define('NFW_RULES_VERSION', '20160416.1');
+define('NFW_ENGINE_VERSION', '3.1.4');
+define('NFW_RULES_VERSION', '20160519.1');
 define('NFW_EDN', 1);
 
 // Set to 0 if you don't want NF to connect to the update server
